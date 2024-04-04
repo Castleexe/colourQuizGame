9769767270a481ad49db4f0423cb76e5e52f3738
Castleexe <dipansh860@gmail.com>
2024-04-03 20:53:39 -0700

    added transitions to next question

--- Colour Clash/Assets/GamaManager.cs
+++ Colour Clash/Assets/GamaManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class GamaManager : MonoBehaviour
 {
@@ -10,21 +12,58 @@ public class GamaManager : MonoBehaviour
 
     private Question currentQuestion;
 
+    [SerializeField] Image questionImage;
+    [SerializeField] float timeBetweenQuestions = 1f;
     private void Start()
     {
         if (unansweredQuestions == null || unansweredQuestions.Count == 0)
         {
             unansweredQuestions = questions.ToList<Question>();
         }
-        GetRandomQuestion();
-        Debug.Log(currentQuestion + " is " + currentQuestion.isTrue);
+        SetCurrentQuestion();
     }
 
-    void GetRandomQuestion()
+    void SetCurrentQuestion()
     {
         int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count());
         currentQuestion = unansweredQuestions[randomQuestionIndex];
 
-        unansweredQuestions.RemoveAt(randomQuestionIndex);
+        questionImage.sprite = currentQuestion.sprite;
+
+        
+    }
+
+    IEnumerator TransitionToNextQuestion ()
+    {
+        unansweredQuestions.Remove(currentQuestion);
+        yield return new WaitForSeconds(timeBetweenQuestions);
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void UserSelectTrue()    
+    {
+        if (currentQuestion.isTrue)
+        {
+            Debug.Log("CORRECT!");
+        }
+        else
+        {
+            Debug.Log("WRONG! BOO HOO");
+        }
+        StartCoroutine(TransitionToNextQuestion());
+    }
+
+    public void UserSelectFalse()
+    {
+        if (!currentQuestion.isTrue)
+        {
+            Debug.Log("CORRECT!");
+        }
+        else
+        {
+            Debug.Log("WRONG! BOO HOO");
+        }
+        StartCoroutine(TransitionToNextQuestion());
     }
 }

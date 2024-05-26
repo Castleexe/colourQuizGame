e57f5b08ff70295664b46688ef749833ad01ea24
Castleexe <dipansh860@gmail.com>
2024-05-25 22:23:03 -0700

    added more images and fixed high score

--- Colour Clash/Assets/Scripts/Score.cs
+++ Colour Clash/Assets/Scripts/Score.cs
@@ -7,7 +7,6 @@ using UnityEngine.SceneManagement;
 public class Score : MonoBehaviour
 {
     static int score = 0;
-    static int highScore = 0;
     [SerializeField] Text scoreText;
     [SerializeField] Text highScoreText;
     // Start is called before the first frame update
@@ -24,7 +23,7 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void addScore()
@@ -40,11 +39,11 @@ public class Score : MonoBehaviour
 
     public void updateHighScore()
     {
-        if (score > highScore)
+        if (score > PlayerPrefs.GetInt("HighScore", 0))
         {
-            highScore = score;
-            highScoreText.text = "High Score: " + highScore.ToString();
-
+            PlayerPrefs.SetInt("HighScore", score);
         }
+        highScoreText.text = "High Score: " + PlayerPrefs.GetInt("HighScore", 0).ToString();
     }
 }
+
